94279eae1136496afc7a093303271d20804b8287
Gerfy <110788311+Gerfy1@users.noreply.github.com>
2024-06-10 12:30:28 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,32 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner buscado = new Scanner(System.in);
         ConsultarCep consultacep = new ConsultarCep();
+        List<Endereco> enderecos = new ArrayList<>();
+        String condicao = "sim";
 
-        System.out.println("Digite o CEP desejada para realizar a consulta de endereço:");
+
+        while (condicao.equalsIgnoreCase("Sim")) {
+        System.out.println("Digite o CEP desejado para realizar a consulta de endereço:");
         String cep = buscado.nextLine();
         try {
             Endereco novoEndereco = consultacep.buscaCEP(cep);
             System.out.println(novoEndereco);
             GerandoJson gerar = new GerandoJson();
-            gerar.gerarJson(novoEndereco);
+            enderecos.add(novoEndereco);
         } catch (Exception e) {
             System.out.println("Ocorreu um erro ao realizar a consulta.\nVerifique se o CEP está correto!");
         }
-
+        System.out.println("Iniciar uma nova consulta?");
+        condicao = buscado.nextLine();
+        }
+        GerandoJson gerar = new GerandoJson();
+        gerar.gerarJson(enderecos);
+        buscado.close();
     }
 }
