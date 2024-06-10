53f73781c40aad13eca428e4a9720558f3c724e0
Gerfy <110788311+Gerfy1@users.noreply.github.com>
2024-06-10 12:30:08 -0300

    Update GerandoJson.java

--- src/GerandoJson.java
+++ src/GerandoJson.java
@@ -3,14 +3,15 @@ import com.google.gson.GsonBuilder;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class GerandoJson {
-    public void gerarJson(Endereco endereco) throws IOException {
+    public void gerarJson(List<Endereco> enderecos) throws IOException {
         Gson gson = new GsonBuilder()
                 .setPrettyPrinting()
                 .create();
         FileWriter registro = new FileWriter("CepsConsultados.json");
-        registro.write(gson.toJson(endereco));
+        registro.write(gson.toJson(enderecos));
         registro.close();
     }
 }
